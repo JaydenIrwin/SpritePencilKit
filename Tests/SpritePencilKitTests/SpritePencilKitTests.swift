83e64dac46d389c9efa55b71351f1a4591a440fd
Jayden Irwin <me@jaydenirwin.com>
2019-09-05 20:39:59 -0400

    1.0.0

--- Tests/SpritePencilKitTests/SpritePencilKitTests.swift
+++ Tests/SpritePencilKitTests/SpritePencilKitTests.swift
@@ -6,7 +6,7 @@ final class SpritePencilKitTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SpritePencilKit().text, "Hello, World!")
+//        XCTAssertEqual(SpritePencilKit().text, "Hello, World!")
     }
 
     static var allTests = [
